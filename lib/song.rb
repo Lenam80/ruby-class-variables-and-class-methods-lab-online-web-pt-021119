7004df20fefe465486357a40601ae9b786948856
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-13 17:09:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,8 @@ attr_accessor :song, :artist, :genre
 
 def intialize
 Lucifer = Song.new ("Lucifer", "Jay-Z", "rap")
+ninety_nine_problems = Song.new ("99 problems", "Jay-Z", "rap")
+hit_me = Song.new = ("Hit me baby one more time", "Brittany Spears", "pop")
 
 def artists
 @@artists = Jay-Z
